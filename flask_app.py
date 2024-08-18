e8a8119a050b182519a3157aeb536555a311e5b2
lewsley-r <lewsley-r@ulster.ac.uk>
2024-08-18 13:10:53 +0100

    moved logic for getting records

--- flask_app.py
+++ flask_app.py
@@ -7,23 +7,22 @@ app = Flask(__name__)
 api = Api(os.getenv('AIRTABLE_API_KEY'))
 table = api.table('app5VgB4Gt5plSkne', 'r7-pool')
 
-records = table.all()
-sorted_records = []
-total = {'Glenn': 0, 'Ronan': 0}
-for record in records:
-    total['Glenn'] += record["fields"]["Glenn"]
-    total['Ronan'] += record["fields"]["Ronan"]
-    record["fields"]["id"] = record["id"]
-    sorted_records.append(record["fields"])
-
 
 @app.route("/", methods=["GET", "POST"])
 def display_scores():
     if request.method == "GET":
+        records = table.all()
+        sorted_records = []
+        total = {'Glenn': 0, 'Ronan': 0}
+        for record in records:
+            total['Glenn'] += record["fields"]["Glenn"]
+            total['Ronan'] += record["fields"]["Ronan"]
+            record["fields"]["id"] = record["id"]
+            sorted_records.append(record["fields"])
         return render_template('Scores.html', scores=sorted_records, total=total)
 
     table.create(
-        {"Ronan": request.form["Ronan"], "Glenn": request.form["Glenn"]})
+        {"Ronan": int(request.form["Ronan"]), "Glenn": int(request.form["Glenn"])})
     return redirect(url_for('display_scores'))
 
 
