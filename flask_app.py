ee7d2cfd7896bb0052fbedab05e42d022de37dc5
lewsley-r <lewsley-r@ulster.ac.uk>
2024-08-31 23:05:19 +0100

    bug fix

--- flask_app.py
+++ flask_app.py
@@ -11,14 +11,14 @@ table = api.table('app5VgB4Gt5plSkne', 'r7-pool')
 @app.route("/", methods=["GET", "POST"])
 def display_scores():
     if request.method == "GET":
-        # records = table.all()
-        # sorted_records = []
-        # total = {'Glenn': 0, 'Ronan': 0}
-        # for record in records:
-        #     total['Glenn'] += record["fields"]["Glenn"]
-        #     total['Ronan'] += record["fields"]["Ronan"]
-        #     record["fields"]["id"] = record["id"]
-        #     sorted_records.append(record["fields"])
+        records = table.all()
+        sorted_records = []
+        total = {'Glenn': 0, 'Ronan': 0}
+        for record in records:
+            total['Glenn'] += record["fields"]["Glenn"]
+            total['Ronan'] += record["fields"]["Ronan"]
+            record["fields"]["id"] = record["id"]
+            sorted_records.append(record["fields"])
         return render_template('new_scores.html', scores=sorted_records, total=total)
         # return render_template('new_scores.html')
     table.create(
