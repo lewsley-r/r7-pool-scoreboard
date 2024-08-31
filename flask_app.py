36be40974f8c7bb603dc7476724fd983b446364a
lewsley-r <lewsley-r@ulster.ac.uk>
2024-08-31 23:43:02 +0100

    added code to pull in monthly winners

--- flask_app.py
+++ flask_app.py
@@ -6,21 +6,27 @@ app = Flask(__name__)
 
 api = Api(os.getenv('AIRTABLE_API_KEY'))
 table = api.table('app5VgB4Gt5plSkne', 'r7-pool')
-
+winners_table = api.table('app5VgB4Gt5plSkne', 'r7-pool-winners')
 
 @app.route("/", methods=["GET", "POST"])
 def display_scores():
     if request.method == "GET":
+        winners = winners_table.all()
         records = table.all()
         sorted_records = []
         total = {'Glenn': 0, 'Ronan': 0}
+        winner_records = []
+        for winner in winners:
+            temp_record = {}             
+            temp_record['Name'] = winner['fields']['Name']
+            temp_record['Month'] = winner['fields']['Month']
+            winner_records.append(temp_record)
         for record in records:
             total['Glenn'] += record["fields"]["Glenn"]
             total['Ronan'] += record["fields"]["Ronan"]
             record["fields"]["id"] = record["id"]
             sorted_records.append(record["fields"])
         return render_template('new_scores.html', scores=sorted_records, total=total)
-        # return render_template('new_scores.html')
     table.create(
         {"Ronan": int(request.form["Ronan"]), "Glenn": int(request.form["Glenn"])})
     return redirect(url_for('display_scores'))
